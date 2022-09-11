932c7c2b8682433de247f78c15f75ab3c5d5b745
simonebonifazi <simonbonifraz@gmail.com>
2022-09-11 05:40:17 +0200

    added_constructor

--- index.php
+++ index.php
@@ -4,17 +4,27 @@ class Movie
 {
     public $title;
     public $year;
+    public $main_actors;
     public $cast;
     public $description;
     public $plot;
     public $duration;
+
+    public function __construct($title, $year, $main_actors, $duration)
+    {
+        $this->title = $title;
+        $this->year = $year;
+        $this->main_actors = $main_actors;
+        $this->duration = $duration;
+    }
 
     public function getMovieInfo()
     {
         return
         "titolo : $this->title 
         anno : $this->year
-        cast : $this->cast 
+        protagonisti : $this->main_actors
+        cast : $this->cast
         trama : $this->plot 
         durata : $this->duration
         descrizione : $this->description";
