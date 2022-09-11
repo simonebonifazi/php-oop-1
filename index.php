c055150225ac6d9cf95b50082c487eacf3f86221
simonebonifazi <simonbonifraz@gmail.com>
2022-09-11 05:34:22 +0200

    added_method

--- index.php
+++ index.php
@@ -2,7 +2,23 @@
 
 class Movie
 {
-    
+    public $title;
+    public $year;
+    public $cast;
+    public $description;
+    public $plot;
+    public $duration;
+
+    public function getMovieInfo()
+    {
+        return
+        "titolo : $this->title 
+        anno : $this->year
+        cast : $this->cast 
+        trama : $this->plot 
+        durata : $this->duration
+        descrizione : $this->description";
+    }
 }
 
 ?>
